8347d8d0f3b92bc0e7b0e8561769162db51bd24a
Idan Regev <idanre1@gmail.com>
2021-10-30 12:43:02 +0000

    draft for android ko

--- install_android_ko_in_azure.sh
+++ install_android_ko_in_azure.sh
@@ -7,3 +7,11 @@ DEB=`ls -1 *$PKG*.deb | head -1`
 ar x $DEB
 mkdir kernel_drivers
 tar -xJf data.tar.xz -C kernel_drivers
+
+BINDER=`find . | grep binder`
+ASHMEM=`find . | grep ashmem`
+#./lib/modules/5.8.0-1041-azure/kernel/drivers/android/binder_linux.ko
+#./lib/modules/5.8.0-1041-azure/kernel/drivers/staging/android/ashmem_linux.ko
+
+# copy to modules and insmod
+#sudo insmod /lib/modules/5.8.0-1041-azure/kernel/drivers/android/binder_linux.ko
