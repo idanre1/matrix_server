0939ff4265d28f69796cf42a7d829164198a1511
Idan Regev <idanre1@gmail.com>
2021-12-20 10:13:20 +0200

    Update matrix_upgrade.sh

--- matrix_upgrade.sh
+++ matrix_upgrade.sh
@@ -4,11 +4,13 @@ aptyes='sudo DEBIAN_FRONTEND=noninteractive apt-get -y '
 $aptyes update
 $aptyes upgrade
 $aptyes dist-upgrade
+sudo systemctl stop mautrix-telegram
 
 cd /opt/mautrix-telegram
 source bin/activate
+sudo -u mautrix-telegram bin/pip install --upgrade mautrix-telegram[all]
+alembic -x config=/opt/mautrix-telegram/config.yaml upgrade head
 
-sudo systemctl stop mautrix-telegram
 sudo systemctl restart matrix-synapse
 sleep 5
 
