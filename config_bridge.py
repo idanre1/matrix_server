acb90c399c52735778760aa6c3d252c81d4b3767
Idan Regev <idanre1@gmail.com>
2022-03-01 09:03:37 +0000

    config with options

--- config_bridge.py
+++ config_bridge.py
@@ -5,8 +5,8 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("-n", "--name", help="server_name")
 parser.add_argument("-p", "--password", help="postgres password")
-parser.add_argument("-i", "--id", help="telegram app id")
-parser.add_argument("--hash", help="telegram app hash")
+parser.add_argument("-i", "--id", default='', action='store', help="telegram app id, leave blank to ignore")
+parser.add_argument("--hash",     default='', action='store', help="telegram app hash, leave blank to ignore")
 parser.add_argument("--bridge", help="Bridge name", default='telegram', action='store')
 args = parser.parse_args()
 
@@ -24,8 +24,14 @@ doc['homeserver']['domain']=args.name
 doc['appservice']['database']=f'postgres://{args.bridge}_user:{args.password}@127.0.0.1/{args.bridge}'
 
 # config
-doc[args.bridge]['api_id']=int(args.id)
-doc[args.bridge]['api_hash']=args.hash
+if args.id != '':
+	doc[args.bridge]['api_id']=int(args.id)
+else:
+	print('No api_id, ignoring...')
+if args.hash != '':
+	doc[args.bridge]['api_hash']=args.hash
+else:
+    print('No api_hash, ignoring...')
 
 # bridge
 perm={}
