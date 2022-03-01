0e4110c83e3694ade52d6e0efb15818079d464a1
Idan Regev <idanre1@gmail.com>
2022-03-01 12:03:17 +0200

    Update install_bridge.sh

--- install_bridge.sh
+++ install_bridge.sh
@@ -48,9 +48,9 @@ sudo -u postgres createdb --encoding=UTF8 --locale=C --template=template0 --owne
 # bridge setup
 sudo -u mautrix-${BRIDGE_NAME} cp /opt/mautrix-${BRIDGE_NAME}/example-config.yaml /opt/mautrix-${BRIDGE_NAME}/config.yaml
 if [[ $PARAMS_N -eq 3 ]]; then
-    python3 config_bridge.py --bridge ${BRIDGE_NAME} --name $DOMAIN -p $BRIDGE_PASS
+    sudo -u mautrix-${BRIDGE_NAME} python3 config_bridge.py --bridge ${BRIDGE_NAME} --name $DOMAIN -p $BRIDGE_PASS
 else
-    python3 config_bridge.py --bridge ${BRIDGE_NAME} --name $DOMAIN -p $BRIDGE_PASS -i $API_ID --hash $API_HASH
+    sudo -u mautrix-${BRIDGE_NAME} python3 config_bridge.py --bridge ${BRIDGE_NAME} --name $DOMAIN -p $BRIDGE_PASS -i $API_ID --hash $API_HASH
 fi
 
 # fold
