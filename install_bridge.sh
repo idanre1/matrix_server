d98c69d0eb003e461ba414924768f59b5b47a834
Idan Regev <idanre1@gmail.com>
2022-03-01 11:56:13 +0200

    Update install_bridge.sh

--- install_bridge.sh
+++ install_bridge.sh
@@ -47,9 +47,9 @@ sudo -u postgres createdb --encoding=UTF8 --locale=C --template=template0 --owne
 
 # bridge setup
 if [[ $PARAMS_N -eq 3 ]]; then
-    /opt/mautrix-${BRIDGE_NAME}/bin/python config_bridge.py --name $DOMAIN -p $BRIDGE_PASS
+    python3 config_bridge.py --name $DOMAIN -p $BRIDGE_PASS
 else
-    /opt/mautrix-${BRIDGE_NAME}/bin/python config_bridge.py --name $DOMAIN -p $BRIDGE_PASS -i $API_ID --hash $API_HASH
+    python3 config_bridge.py --name $DOMAIN -p $BRIDGE_PASS -i $API_ID --hash $API_HASH
 fi
 
 # fold
