c0c8ce42d8b02dac26e119d2247ead45edbf1f63
Idan Regev <idanre1@gmail.com>
2022-03-02 11:13:25 +0200

    Update run_matrix_cron.sh

--- run_matrix_cron.sh
+++ run_matrix_cron.sh
@@ -8,9 +8,18 @@ echo "*** matrix_server cron started"
 # Main
 # ----------------------------------------
 
+# certbot
+echo "*** certbot - start"
+/usr/bin/certbot renew --quiet
+echo "*** certbot - end"
+
+sleep 30
+
 # Purge old media
+echo "*** Purging old media - start"
 # https://github.com/matrix-org/synapse/issues/2315
 find /var/lib/matrix-synapse/media -type f -size +5M -mtime +120 -exec rm -iv {} \; # size >5MB, older than 120 days
+echo "*** Purging old media - end"
 
 # ----------------------------------------
 # Footer
