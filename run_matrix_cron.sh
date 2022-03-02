fc3957e93e7c5a59d69c69c0c870144aabe6dde9
Idan Regev <idanre1@gmail.com>
2022-03-02 11:09:58 +0200

    Update run_matrix_cron.sh

--- run_matrix_cron.sh
+++ run_matrix_cron.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+# ----------------------------------------
+# Header
+# ----------------------------------------
 echo "*** matrix_server cron started"
 
+# ----------------------------------------
+# Main
+# ----------------------------------------
+
+# Purge old media
+# https://github.com/matrix-org/synapse/issues/2315
+find /var/lib/matrix-synapse/media -type f -size +5M -mtime +120 -exec rm -iv {} \; # size >5MB, older than 120 days
+
+# ----------------------------------------
+# Footer
+# ----------------------------------------
 echo "*** matrix_server cron done"
